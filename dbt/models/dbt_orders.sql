d0264857948fe5cb07a2caf9daa0f03c6f429704
Ian Ahuja <12ian34@gmail.com>
2022-06-23 10:18:18 +0100

    update shipping address type to text

--- dbt/models/dbt_orders.sql
+++ dbt/models/dbt_orders.sql
@@ -1,5 +1,4 @@
-SELECT
-  ord.order_id,
+SELECT ord.order_id,
   ord.order_date,
   ord.partner_id,
   prt.partner_name,
@@ -12,17 +11,9 @@ SELECT
   usr.email,
   usr.created_date AS user_created_date,
   usr.browser,
-  usr.shipping_address::json->'city' AS shipping_city,
-  usr.shipping_address::json->'country' AS shipping_country
-FROM
-  thyme.orders ord
-LEFT JOIN
-  thyme.users usr
-ON
-  ord.user_id = usr.user_id
-LEFT JOIN
-  thyme.partners prt
-ON
-  ord.partner_id = prt.partner_id
-ORDER BY
-  CAST(order_id AS int) ASC
+  cast(usr.shipping_address::json->'city' as text) AS shipping_city,
+  cast(usr.shipping_address::json->'country' as text) AS shipping_country
+FROM thyme.orders ord
+  LEFT JOIN thyme.users usr ON ord.user_id = usr.user_id
+  LEFT JOIN thyme.partners prt ON ord.partner_id = prt.partner_id
+ORDER BY CAST(order_id AS int) ASC

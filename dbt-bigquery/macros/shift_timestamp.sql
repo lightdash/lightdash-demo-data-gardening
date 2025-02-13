d3d4f41dbe7c059ba9e91baf7f1ac2349f819472
Oli Ramsay <oliver.ramsay@lightdash.com>
2025-02-13 01:24:44 +0100

    finalising the output data for big query

--- dbt-bigquery/macros/shift_timestamp.sql
+++ dbt-bigquery/macros/shift_timestamp.sql
@@ -1,5 +1,5 @@
 
-{% macro shift_timestamp(date_col, max_tstamp = '2024-05-28 00:00:00') %}
+{% macro shift_timestamp(date_col, max_tstamp = '2025-02-12 00:00:00') %}
   timestamp_add(
     {{ date_col }},
     interval timestamp_diff(current_timestamp(), timestamp '{{ max_tstamp }}', second) second

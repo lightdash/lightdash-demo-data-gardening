b2d726f1f0659f3037fa36f09fee57a10a0ef6db
Jake Peterson <jake@lightdash.com>
2025-11-04 15:52:08 -0600

    improve model format and add PoP param example

--- dbt-bigquery/models/dbt_orders.sql
+++ dbt-bigquery/models/dbt_orders.sql
@@ -1,26 +1,24 @@
-{{
-  config(
-    tags = ['not_lightdash']
-  )
-}}
+select 
+  cast(orders.order_id as string) as order_id,
+  orders.order_date,
+  cast(orders.partner_id as string) as partner_id,
+  partners.partner_name,
+  partners.partner_commission,
+  orders.currency,
+  orders.basket_total,
+  orders.profit,
+  orders.referrer,
+  cast(orders.user_id as string) as user_id,
+  users.email,
+  users.created_date as user_created_date,
+  users.browser,
+  json_extract_scalar(shipping_address, '$.city') as shipping_city,
+  json_extract_scalar(shipping_address, '$.country') as shipping_country
 
-SELECT 
-  CAST(ord.order_id AS STRING) AS order_id,
-  ord.order_date,
-  CAST(ord.partner_id AS STRING) AS partner_id,
-  prt.partner_name,
-  prt.partner_commission,
-  ord.currency,
-  ord.basket_total,
-  ord.profit,
-  ord.referrer,
-  CAST(ord.user_id AS STRING) AS user_id,
-  usr.email,
-  usr.created_date AS user_created_date,
-  usr.browser,
-  JSON_EXTRACT_SCALAR(shipping_address, '$.city') AS shipping_city,
-  JSON_EXTRACT_SCALAR(shipping_address, '$.country') AS shipping_country
-FROM {{ ref('orders') }} ord
-  LEFT JOIN {{ ref('users') }} usr ON CAST(ord.user_id AS STRING) = CAST(usr.user_id AS STRING)
-  LEFT JOIN {{ ref('partners') }} prt ON CAST(ord.partner_id AS STRING) = CAST(prt.partner_id AS STRING)
-ORDER BY CAST(order_id AS int) ASC
+from {{ ref('orders') }} as orders
+
+  left join {{ ref('users') }} as users 
+    on cast(orders.user_id as string) = cast(users.user_id as string)
+
+  left join {{ ref('partners') }} as partners
+    on cast(orders.partner_id as string) = cast(partners.partner_id as string)
